1f5bae6c17be4e49d31ad2ecd3e832c06995d754
ThiriYadanar <tryatanar@gmail.com>
2022-02-14 17:28:00 +0630

    true test case

--- src/main/java/com/example/demo/HelloService.java
+++ src/main/java/com/example/demo/HelloService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class HelloService {
 
     public String hello() {
-        return "";
+        return "hello";
     }
     
     public String hi() {
